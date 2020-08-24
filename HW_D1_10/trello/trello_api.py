e2672aa4f1c4d4b8bf470e0e03412edf61b3cb7d
gandzytm <gandzy1984@gmail.com>
2020-08-24 13:23:07 +0500

    HW D1.10

--- HW_D1_10/trello/trello_api.py
+++ HW_D1_10/trello/trello_api.py
@@ -1,6 +1,3 @@
-import json
-
-import pandas as pd
 import sys
 import requests
 
@@ -12,11 +9,38 @@ auth_params = {
 base_url = "https://api.trello.com/1/{}"
 board_id = 'D0QtTj1P'
 
+# name текущее название колонки
+def update_label(column_id, name):
+    task_data = requests.get(base_url.format('lists') + '/' + column_id + '/cards', params=auth_params).json()
+    print(task_data)
+    arr_name = name.split(' ')
+    print(arr_name)
+    new_label = str(len(task_data)) + name[len(arr_name[0]):] if arr_name[0].isdigit() else str(
+        len(task_data)) + ' ' + name
+    print(new_label)
+    requests.put(base_url.format('lists') + '/' + column_id, params={'name': new_label, **auth_params})
+
+
+# название колонки без цифры
+def get_column_name(name):
+    arr_name = name.split(' ')
+    name = ' '.join(arr_name[1:])
+    return name
+
+
 def read():
-    column_data = requests.get(base_url.format('boards' + '/' + board_id + '/lists'), params=auth_params).json()
+    # Получим данные всех колонок на доске:
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+
+    # Теперь выведем название каждой колонки и всех заданий, которые к ней относятся:
     for column in column_data:
+        print(column['name'])
+        # Получим данные всех задач в колонке и перечислим все названия
         task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
-        print(column['name'] + ' ' + str(len(task_data)) + ' задачи')
+
+        # добавляем в название колонки к-во задач (карточек) в ней
+        update_label(column['id'], column['name'])
+
         if not task_data:
             print('\t' + 'Нет задач!')
             continue
@@ -24,32 +48,66 @@ def read():
             print('\t' + task['name'])
 
 
+# Создадим задачу (добавим карточку) с названием name в колонке с названием column_name
 def create(name, column_name):
+    # Получим данные всех колонок на доске
     column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
 
+    # Переберём данные обо всех колонках, пока не найдём ту колонку, которая нам нужна
     for column in column_data:
-        if column['name'] == column_name:
+        if get_column_name(column['name']) == column_name:  # убираем к-во задач в названии колонки
+            # Создадим задачу с именем _name_ в найденной колонке
             requests.post(base_url.format('cards'), data={'name': name, 'idList': column['id'], **auth_params})
+            update_label(column['id'], column['name'])  # обновляем к-во задач в названии колонки
             break
 
 
+# column_name  в какую колонку переместить
 def move(name, column_name):
-    column_data = requests.get(base_url.format('boards' + '/' + board_id + '/lists'), params=auth_params).json()
+    # Получим данные всех колонок на доске
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
 
+    # Среди всех колонок нужно найти задачу по имени и получить её id
+    arr_tasks = []  # Сформировать список из словарей с данными по задачи с введенным названием
     task_id = None
     for column in column_data:
         column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
         for task in column_tasks:
             if task['name'] == name:
-                task_id = task['id']
-                break
-        if task_id:
+                arr_tasks.append(
+                    {'id': task['id'], 'idShort': task['idShort'], 'idList': task['idList'], 'column': column['name']})
+
+    for i in range(len(arr_tasks)):
+        print('Порядковый номер {}) Задача с идентификатором "{}" из колонки с названием "{}"'.format(i, arr_tasks[i][
+            'id'], arr_tasks[i]['column']))
+
+    arr_tasks_index = int(input('Введите порядковый номер задачи для дальнейшей обработки \n'))
+    task_id = arr_tasks[arr_tasks_index]['id']
+    column_id = arr_tasks[arr_tasks_index]['idList']  # из этой колонки будем удалять карточку
+    column_old = arr_tasks[arr_tasks_index]['column']
+
+    # Теперь, когда у нас есть id задачи, которую мы хотим переместить
+    # Переберём данные обо всех колонках, пока не найдём ту, в которую мы будем перемещать задачу
+    for column in column_data:
+        if get_column_name(column['name']) == column_name:
+            # И выполним запрос к API для перемещения задачи в нужную колонку
+            requests.put(base_url.format('cards') + '/' + task_id + '/idList',
+                         data={'value': column['id'], **auth_params})
+            update_label(column['id'], column['name'])  # Обновляем к-во задач в названии колонки, куда добавили задачу
+            update_label(column_id, column_old)  # Обновляем к-во задач в названии колонки, откуда удалили задачу
+            print('Задача "{}" успешно перемещена из колонки "{}" в колонку "{}"'.format(name,
+                                                                                         get_column_name(column_old),
+                                                                                         column_name))
             break
 
-    for column in column_data:
-        if column['name'] == column_name:
-            requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column['id'], **auth_params})
-            break
+
+# добавление новой колонки
+# "https://api.trello.com/1/boards/{id}/lists"
+def create_column(name):
+    response = requests.post(base_url.format('boards') + '/' + board_id + '/lists',
+                             params={'name': name, **auth_params}).json()
+    update_label(response['id'], name)
+    print('create new columns "{}"'.format(name))
 
 
 if __name__ == "__main__":
@@ -59,13 +117,5 @@ if __name__ == "__main__":
         create(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == 'move':
         move(sys.argv[2], sys.argv[3])
-
-    json_string = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
-    print(json_string)
-    for i in json_string:
-        # a_json = json.loads(i)
-        # print(a_json)
-        dataFrame = pd.DataFrame.from_dict(i, columns=['name'], orient="index")
-    print(dataFrame)
-
-    # print(requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).text)
+    elif sys.argv[1] == 'create_column':
+        create_column(sys.argv[2])
